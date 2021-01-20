7aa949b59ae76dc3719f52c5d2455f3cbf4f47a1
israr <isrargul00@gmail.com>
2021-01-21 03:55:30 +0500

    bbwa

--- core/views.py
+++ core/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("welcome to web bsae application")
+    #return HttpResponse("welcome to web bsae application")
+    return render(request , 'base.html')
